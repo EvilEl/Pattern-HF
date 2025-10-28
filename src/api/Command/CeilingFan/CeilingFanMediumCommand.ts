749633c3a48f63387a6227b3f770cd1ea328a53c
Michael Andreev <enc0ru@Rambler.ru>
2025-10-28 17:52:28 +0300

    refactor: реализация методов execute() и undo() в CeilingFanMediumCommand

--- src/api/Command/CeilingFan/CeilingFanMediumCommand.ts
+++ src/api/Command/CeilingFan/CeilingFanMediumCommand.ts
@@ -1,12 +1,28 @@
 import type { Command } from "../Command"
-import type { CeilingFan } from "./CeilingFan"
+import { ENUM_CEILING_FAN_SPEED, type CeilingFan } from "./CeilingFan"
 
-export class CeilingFanMediumCommand implements Command {
-    ceilingFan: CeilingFan
-    prevSpeed: number = 0
+export class CeilingFanMediumCommand implements Command  {
+  ceilingFan: CeilingFan
+  prevSpeed: number = 0
 
-    constructor(ceilingFan:CeilingFan){
-        this.ceilingFan = ceilingFan  
+  constructor(ceilingFan:CeilingFan){
+    this.ceilingFan = ceilingFan
+  }
+
+  execute(){
+    this.prevSpeed = this.ceilingFan.getSpeed()
+    this.ceilingFan.low()
+  }
+
+  undo(){
+    if(this.prevSpeed === ENUM_CEILING_FAN_SPEED.OFF){
+      this.ceilingFan.off()
+    }else if(this.prevSpeed === ENUM_CEILING_FAN_SPEED.LOW){
+      this.ceilingFan.low()
+    }else if(this.prevSpeed === ENUM_CEILING_FAN_SPEED.MEDIUM){
+      this.ceilingFan.medium()
+    }else if(this.prevSpeed === ENUM_CEILING_FAN_SPEED.HIGH){
+      this.ceilingFan.high()
     }
-
+  }
 }
